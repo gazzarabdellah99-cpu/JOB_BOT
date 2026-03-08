0c14f942e876d6b1153cc60a1b43091d860d28b9
JOB-BOT-MASTER <chaima.seiff@gmail.com>
2026-03-08 01:30:43 +0100

    Create scraper-site1.py

---
+++ scraper/scraper-site1.py
@@ -0,0 +1,23 @@
+"""
+scraper_site1.py - Groupe 2 : Scraping du site 1
+
+Format de retour attendu par bot.py :
+[
+    {
+        "title": "Titre du poste",
+        "company": "Nom de l'entreprise",
+        "location": "Ville",
+        "description": "Description complète",
+        "url": "lien vers l'offre"
+    },
+    ...
+]
+"""
+
+
+def scrape_offers(job_type: str, location: str) -> list:
+    """
+    TODO Groupe 2 : implémenter avec Selenium
+    """
+    offers = []
+    return offers
